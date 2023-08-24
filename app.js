57bae718914109940333e31e7cd5060fa84a0c6f
shahin-alamm <shahinalamr27@gmail.com>
2023-08-24 10:40:46 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+
+// function getFullName ( firstName , lastName){
+//     console.log(arguments);
+//  let fullName ='';
+//  for(i=0; i<arguments.length; i++){
+//     const namePart = arguments[i];
+//     fullName = fullName + ' ' + namePart;
+//  }
+//     return fullName;
+// }
+
+// const name = getFullName('shahin', 'alam', ' programing ', 'bujhe na ' )
+// console.log(name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('case-increase').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-count')
+    console.log(caseInput.value);
+})
